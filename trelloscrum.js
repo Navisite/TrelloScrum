bc6851b85e372b18597e1bce3cafd35d61447360
Kelby Valenti <kvalenti@navisite.com>
2012-07-25 11:06:30 -0400

    Updated points

--- trelloscrum.js
+++ trelloscrum.js
@@ -16,7 +16,7 @@
 */
 
 //default story point picker sequence
-var _pointSeq = ['?', '.5', 0, 1, 2, 4, 8, 16, 32, 64];
+var _pointSeq = [0, '.5', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
 //internals
 var filtered = false, //watch for filtered cards
